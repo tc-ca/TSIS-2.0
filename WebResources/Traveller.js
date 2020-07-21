2fdcc4e4271a0328abef4d404df2c994765317f9
Weiguang <weiguang.xiao@034gc.onmicrosoft.com>
2020-07-21 15:06:19 -0400

    Formatted the js file

--- WebResources/Traveller.js
+++ WebResources/Traveller.js
@@ -1,3 +1,4 @@
+﻿window.parentExecutionContext = null;
 window.parentFormContext = null;
 
 function InitializeButton(eContext) {
@@ -9,16 +10,31 @@ function InitializeButton(eContext) {
 function proceed() {
   var eContext = window.parentExecutionContext;
   var formContext = eContext.getFormContext();
+  formContext.getAttribute('ppp_matchfoundtime').setValue(new Date());
+  formContext.getControl('ppp_matchfound').setVisible(false);
+  formContext.getControl('WebResource_traveller').setVisible(false);
   ToggleTabs();
+  formContext.ui.tabs.get('tab_DetailedTravellerInformation').setFocus();
 }
 
 function ToggleTabs() {
   var eContext = window.parentExecutionContext;
   var formContext = eContext.getFormContext();
+  var confirmation = formContext.getAttribute('ppp_matchfoundconfirmation');
   confirmation.setValue(true);
   formContext.data.save();
   //confirmation.setSubmitMode("Always");
 
+  ShowHideTabs(eContext, 'ppp_matchfound', 927820001, [
+    'tab_TravelInformation',
+    'tab_TravellerInformation',
+  ]);
+  ShowHideTabs(eContext, 'ppp_matchfound', 927820000, [
+    'tab_DetailedTravellerInformation',
+    'tab_TravelDetails',
+    'tab_RecommendedAction',
+    'tab_Decision',
+  ]);
 }
 
 function OnLoad(eContext) {
@@ -26,6 +42,7 @@ function OnLoad(eContext) {
   var formContext = eContext.getFormContext();
 
   // Get the web resource control on the form
+  var wrCtrl = formContext.getControl('WebResource_traveller');
   // Get the web resource inner content window
   if (wrCtrl !== null && wrCtrl !== undefined) {
     wrCtrl.getContentWindow().then(function (win) {
@@ -50,6 +67,7 @@ function populateCurrentUser(eContext, userFieldName) {
 
   var userField = formContext.getAttribute(userFieldName);
   userField.setValue(currentUser);
+  userField.setSubmitMode('always');
 }
 
 //function populateContactInfo(eContext, repFieldName, phoneFieldName) {
@@ -76,15 +94,20 @@ function populateCurrentUser(eContext, userFieldName) {
 
 //};
 
+// Prefill a Date Field with Todays Date
 function SetCallTime(eContext, fieldName) {
+  var formContext = eContext.getFormContext();
   var isCreateForm = formContext.ui.getFormType() == 1;
+  if (!isCreateForm) return;
   SetNow(eContext, fieldName);
 }
 
 function SetNow(eContext, fieldName) {
+  var formContext = eContext.getFormContext();
   var field = formContext.getAttribute(fieldName);
   if (field == null || field == 'undefined') return;
   field.setValue(new Date()); // Set the Date field to Today
+  field.setSubmitMode('always'); // Save Disabled Fields
 }
 
 function ShowHideWebResource(eContext, fieldName, value, webResource) {
@@ -97,6 +120,7 @@ function ShowHideWebResource(eContext, fieldName, value, webResource) {
 
   if (field.getValue() == null) {
     isVisible = false;
+  } else if (field.getValue() == value) {
     isVisible = true;
   }
 
@@ -110,6 +134,7 @@ function ShowHideWebResource(eContext, fieldName, value, webResource) {
   //    win.InitializeButton(eContext);
   //  });
   //}
+}
 
 function ShowHideTabs(eContext, fieldName, value, tabs) {
   var formContext = eContext.getFormContext();
@@ -121,6 +146,7 @@ function ShowHideTabs(eContext, fieldName, value, tabs) {
 
   if (field.getValue() == null) {
     isVisible = false;
+  } else if (field.getValue() == value) {
     isVisible = true;
   }
 
@@ -129,6 +155,7 @@ function ShowHideTabs(eContext, fieldName, value, tabs) {
     if (tab == null || tab == 'undefined') return;
     tab.setVisible(isVisible);
   }
+}
 //'avs_name',
 //  ['tab_FlightInformation', 'tab_PassengerInfo']
 
@@ -148,12 +175,16 @@ function ShowHideTextbox(eContext, optFieldName, value, txtFieldName) {
 
   if (optField.getValue() == null) {
     isVisible = false;
+  } else if (optField.getValue().includes(value)) {
     isVisible = true;
   }
 
   txtField.setVisible(isVisible);
 
   if (isVisible) {
+    targetField.setRequiredLevel('required');
+  } else {
+    targetField.setRequiredLevel('none');
   }
 }
 
@@ -168,24 +199,54 @@ function DisableSubgrid(eContext, gridName) {
   subGridCtrl.setDisabled(true);
 }
 
+function DisableGrid() {
+  return false;
+}
+
 //Toggles display of the BPF. Takes the formcontext and the the name of the twoOption field that was changed as a string.
 function showHideBusinessProcessFlow(eContext, twoOptionFieldName) {
+  var formContext = eContext.getFormContext();
+  var twoOptionFieldValue = formContext
+    .getAttribute(twoOptionFieldName)
+    .getValue();
 
+  if (twoOptionFieldValue == null || twoOptionFieldValue == 'undefined') return;
+  formContext.ui.process.setVisible(twoOptionFieldValue); //Shows BPF if twoOptionFieldValue is true, hides if false
 }
 
 //Sets the record status to the given status value
 function setRecordStatus(eContext, statusValue) {
+  var formContext = eContext.getFormContext();
+  formContext.getAttribute('ppp_recordstatus').setValue(statusValue);
 }
 
 //Changes status to In-Progress only if passenger is present and current status is Draft
 function statusChangeInProgress(eContext) {
+  var formContext = eContext.getFormContext();
+  var isPresent = formContext.getAttribute('ppp_ispresent').getValue();
+  var isDraft =
+    formContext.getAttribute('ppp_recordstatus').getValue() == 927820001;
+
+  if (isPresent && isDraft) {
+    setRecordStatus(eContext, 927820003);
+  }
 }
 
 //Shows Passport Number if Passport was selected as an ID, hides if not
 function showHidePassportNumber(eContext) {
+  var formContext = eContext.getFormContext();
+  var idUsedArray = formContext.getAttribute('ppp_idsused').getValue();
+  var passportSelected = idUsedArray.includes(927820000);
+  formContext.getControl('ppp_passportnumber').setVisible(passportSelected);
 }
 
 //If the valueField matched the desiredValue, set the given timeField to Now
+function setTimeFieldNow(
+  eContext,
+  valueFieldName,
+  desiredValue,
+  timeFieldName
+) {
   var formContext = eContext.getFormContext();
   var valueField = formContext.getAttribute(valueFieldName).getValue();
   if (valueField == desiredValue) {
@@ -193,6 +254,13 @@ function showHidePassportNumber(eContext) {
   }
 }
 
+function setDateTime(
+  eContext,
+  dateFieldName,
+  hourFieldName,
+  minuteFieldName,
+  dateTimeField
+) {
   var formContext = eContext.getFormContext();
   var date = formContext.getAttribute(dateFieldName).getValue();
   var hour = formContext.getAttribute(hourFieldName).getValue();
@@ -207,20 +275,52 @@ function showHidePassportNumber(eContext) {
 
 function showHideMatchConfirmed(eContext) {
   var formContext = eContext.getFormContext();
+  var firstName = formContext.getAttribute('ppp_firstname').getValue();
+  var lastName = formContext.getAttribute('ppp_lastname').getValue();
+  var gender = formContext.getAttribute('ppp_gender').getValue();
+  var dateOfBirth = formContext.getAttribute('ppp_dateofbirth').getValue();
+  var isPresent = formContext.getAttribute('ppp_ispresent').getValue();
 
   if (firstName && lastName && gender && dateOfBirth && isPresent) {
+    formContext.getControl('ppp_matchfound').setVisible(true);
   } else {
+    formContext.getControl('ppp_matchfound').setVisible(false);
+    formContext.getAttribute('ppp_matchfound').setValue(null);
   }
 }
 
 function ReadOnlyOnClosed(eContext) {
   var formContext = eContext.getFormContext();
+  var recordStatus = formContext.getAttribute('ppp_recordstatus').getValue();
+  var recordClosed = recordStatus == 927820002 || recordStatus == 927820005;
   formContext.ui.controls.forEach(function (attribute) {
     var control = formContext.getControl(attribute.getName());
     if (control) {
+      control.setDisabled(recordClosed);
     }
   });
+  formContext.getControl('header_ppp_recordstatus').setDisabled(false);
+
+  //Disable the add existing buttons on all grids.
+  RefreshGridRibbon(formContext, 'gridCallHistory');
+  RefreshGridRibbon(formContext, 'gridFlightConnections');
+  RefreshGridRibbon(formContext, 'gridFlightConnections2');
 }
 
+function RefreshGridRibbon(formContext, gridName) {
+  var gridContext = formContext.getControl(gridName);
+  if (gridContext == null || gridContext == 'undefined') return;
+  gridContext.refreshRibbon();
+}
 
+function recordIsNotClosed(primaryControl) {
+  var formContext = primaryControl;
+  var recordStatus = formContext.getAttribute('ppp_recordstatus').getValue();
 
+  return !(recordStatus == 927820002 || recordStatus == 927820005);
+}
+
+function refreshConnectionGrid() {
+  //refreshRibbon();
+  //formContext.ui.refreshRibbon();
+}
